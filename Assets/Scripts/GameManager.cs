4fe39ef164bac9ecb3e4512c95647bd6856e05e7
Sourodeep Sarkar <sarkarsourodeep11@gmail.com>
2024-01-28 18:52:51 +0530

    setup game over and new game logic and UI

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,12 +1,19 @@
-using Unity.VisualScripting;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
   public static GameManager Instance;
   public float initialGameSpeed = 5f;
-  public float gameSpeedIncrease = 0.1f;
+  public float gameSpeedIncrease = 0.2f;
   public float gameSpeed { get; private set; }
+
+  private Player player;
+  private Spawner spawner;
+
+  public TextMeshProUGUI gameOverText;
+  public Button retryButton;
 
   private void Awake()
   {
@@ -24,16 +31,44 @@ public class GameManager : MonoBehaviour
 
   private void Start()
   {
-    NewGame();
-  }
+    player = FindAnyObjectByType<Player>();
+    spawner = FindAnyObjectByType<Spawner>();
 
-  private void NewGame()
-  {
-    gameSpeed = initialGameSpeed;
+    NewGame();
   }
 
   private void Update()
   {
     gameSpeed += gameSpeedIncrease * Time.deltaTime;
   }
+
+  public void NewGame()
+  {
+    Obstacle[] obstacles = FindObjectsByType<Obstacle>(FindObjectsSortMode.None);
+
+    foreach (var obs in obstacles)
+    {
+      Destroy(obs.gameObject);
+    }
+
+    gameSpeed = initialGameSpeed;
+    enabled = true;
+
+    player.gameObject.SetActive(true);
+    spawner.gameObject.SetActive(true);
+    gameOverText.gameObject.SetActive(false);
+    retryButton.gameObject.SetActive(false);
+  }
+
+  public void GameOver()
+  {
+    gameSpeed = 0f;
+    enabled = false;
+
+    player.gameObject.SetActive(false);
+    spawner.gameObject.SetActive(false);
+    gameOverText.gameObject.SetActive(true);
+    retryButton.gameObject.SetActive(true);
+  }
+
 }
